c27b4e0e58574e1af185972f83d0bf128b8a38c1
Chris Mems <christopher.mems@.com>
2022-12-05 16:36:02 -0600

    replace component with element

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import NasaPhoto from './components/NasaPhoto';
 import NavBar from './components/NavBar';
@@ -6,12 +6,10 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Route component={Home} path='/' exact />
-        <Route component={NasaPhoto} path='/nasaphoto' />
-      </div>
-    </ Router>
+    <Routes>
+      <Route element={<Home />} path="/" exact />
+      <Route element={<NasaPhoto />} path='/nasaphoto' />
+    </Routes>
   );
 }
 
