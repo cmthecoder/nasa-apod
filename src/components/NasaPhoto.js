87e29712fbf6055c3bf7a7bf2c158af2493c26ee
Chris Mems <christopher.mems@.com>
2022-12-05 19:02:09 -0600

    add information from api

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,6 +1,50 @@
+import React from "react";
+import { useState, useEffect } from "react";
+const apiKey = process.env.REACT_APP_NASA_KEY
+
 const NasaPhoto = () => {
+  const [photoData, setPhotoData] = useState(null);
+
+  useEffect(() => {
+    fetchPhoto()
+
+
+    async function fetchPhoto() {
+      const res = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=${apiKey}
+        `
+      )
+      const data = await res.json()
+      setPhotoData(data)
+      console.log(data)
+    }
+  }, [])
+
+  if (!photoData) return <div />
+
   return (
-    <></>
+    <div>
+      {photoData.media_type === 'image' ? (
+      <img src={photoData.url} alt={photoData.title} />
+      ) : (
+        <iframe
+        title="space-video"
+        src={photoData.url}
+        frameBorder='0'
+        gesture='media'
+        allow='encrypted-media'
+        allowFullScreen
+        className='photo'
+        />
+      )}
+
+      <div>
+        <h1>{photoData.title}</h1>
+        <p>{photoData.data}</p>
+        <p>{photoData.explanation}</p>
+
+      </div>
+    </div>
   );
 }
 
